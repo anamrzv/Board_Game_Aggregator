22fc7d2e875609694064d2b92b5ad28c872c3bb5
Dasxunya <79106085+Dasxunya@users.noreply.github.com>
2023-01-19 23:58:50 +0300

    commit before sleep

--- front/src/views/Cart.vue
+++ front/src/views/Cart.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>привет</div>
+  <div>
+    <div id="header">
+      <div class="icons_container">
+        <div class="icon" id="back_icon">
+          <button @click="goHome" title="homepage"></button>
+        </div>
+        <div class="icon" id="logout_icon">
+          <button @click="logout" title="logout"></button>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
@@ -9,6 +22,19 @@ export default {
     return {
       game_info: null
     }
+  },
+  methods: {
+    goHome() {
+      this.$router.push({name: "main"})
+    },
+    logout() {
+      this.$swal.fire({
+        icon: "success",
+        text: "До встречи!",
+        title: "Вы успешно вышли",
+      });
+      this.$router.push({name: "auth-page"}, () => localStorage.setItem('jwt', null));
+    },
   }
 }
 </script>
