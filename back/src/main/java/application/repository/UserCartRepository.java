545193f10c50b114e0d30a653da6985d7b4b22a1
Ana <ana.mrzv@mail.ru>
2023-01-20 16:39:13 +0300

    fix DELETE issue

--- back/src/main/java/application/repository/UserCartRepository.java
+++ back/src/main/java/application/repository/UserCartRepository.java
@@ -1,9 +1,13 @@
 package application.repository;
 
+import application.domain.Game;
+import application.domain.User;
 import application.domain.UserCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserCartRepository extends JpaRepository<UserCart, Integer> {
+
+    UserCart getUserCartByUserAndGame(User user, Game game);
 }
--- back/src/main/java/application/repository/UserFavRepository.java
+++ back/src/main/java/application/repository/UserFavRepository.java
@@ -1,10 +1,14 @@
 package application.repository;
 
+import application.domain.Game;
+import application.domain.User;
 import application.domain.UserFav;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserFavRepository extends JpaRepository<UserFav, Integer> {
+
+    UserFav getUserFavByUserAndGame(User user, Game game);
 
 }
