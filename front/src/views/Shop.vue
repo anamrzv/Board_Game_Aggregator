790ef8b426689d23264d41829ff50c26c5ddddf5
Dasxunya <79106085+Dasxunya@users.noreply.github.com>
2023-01-21 03:05:55 +0300

    shop begin

--- front/src/views/Shop.vue
+++ front/src/views/Shop.vue
@@ -7,9 +7,9 @@
       <div class="signup">
         <form>
           <label class="unfocus__login">Login</label>
-          <input type="text" name="txt" placeholder="Shop name" required="">
-          <input type="password" name="pswd" placeholder="Password" required="">
-          <button>Login</button>
+          <input type="text" name="txt" placeholder="Shop name" required="" v-model.trim="getLogin">
+          <input type="password" name="pswd" placeholder="Password" required="" v-model.trim="getPassword">
+          <button type="submit" @click="loging">Login</button>
         </form>
       </div>
     </div>
@@ -20,9 +20,30 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Shop",
+  data() {
+    return {
+      getLogin: null,
+      getPassword: null
+    }
+  },
   methods: {
+    loging(e) {
+      e.preventDefault();
+      axios.post('http://localhost:8083/game_aggregator/auth/shop', {
+        login: this.getLogin,
+        password: this.getPassword
+      }).then(response => {
+        localStorage.setItem("shop_login", this.$data.getLogin);
+        localStorage.setItem("shop_jwt", response.data);
+        this.$router.push({name: 'shop-own'});
+      }).catch(error => {
+        document.getElementById("forError").innerText = error.response.data;
+      })
+    },
     goHome() {
       this.$router.push({name: "auth-page"})
     }
